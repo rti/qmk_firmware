ad0890841411fbfc284d20b39bdbdb39fc683355
Robert Timm <mail@rtti.de>
2023-04-17 20:27:59 +0200

    feat: nix shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "QMK dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = (import nixpkgs) {
+        system = "x86_64-linux";
+      };
+
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          qmk
+          python3
+          gcc-arm-embedded
+        ];
+
+        shellHook = ''
+          echo
+          ${pkgs.figlet}/bin/figlet "QMK dev shell"
+          echo
+        '';
+      };
+    };
+}
